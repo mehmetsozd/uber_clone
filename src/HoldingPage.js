7e1bcf13993b615d27aba8119d133d03806be1e7
proximaanalitik <101429433+proximaanalitik@users.noreply.github.com>
2024-03-14 12:19:10 +0300

    favorite components

--- src/HoldingPage.js
+++ src/HoldingPage.js
@@ -5,17 +5,25 @@ import * as React from 'react';
 import HomeScreen from "./screens/HomeScreen";
 import MapScreen from "./screens/MapScreen";
 import EatsScreen from "./screens/EatsScreen";
+import { KeyboardAvoidingView, Platform } from "react-native";
 
 
 const Stack = createNativeStackNavigator();
 
 function MainFeedScreen() {
  return (
-  <Stack.Navigator>
-   <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false }} />
-   <Stack.Screen name="MapScreen" component={MapScreen} options={{ headerShown: false }} />
-   <Stack.Screen name="EatsScreen" component={EatsScreen} options={{ headerShown: false }} />
-  </Stack.Navigator>
+  <KeyboardAvoidingView
+   style={{ flex: 1 }}
+   behavior={Platform.OS === "ios" ? "padding" : "height"}
+   keyboardVerticalOffset={Platform.OS === "ios" ? 0 : 20}
+  >
+   <Stack.Navigator>
+    <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false }} />
+    <Stack.Screen name="MapScreen" component={MapScreen} options={{ headerShown: false }} />
+    <Stack.Screen name="EatsScreen" component={EatsScreen} options={{ headerShown: false }} />
+   </Stack.Navigator>
+  </KeyboardAvoidingView>
+
  );
 }
 
--- src/components/NavigateCard.js
+++ src/components/NavigateCard.js
@@ -1,4 +1,4 @@
-import { SafeAreaView, Text, View, StyleSheet } from 'react-native'
+import { SafeAreaView, Text, View, StyleSheet, TouchableOpacity } from 'react-native'
 import React from 'react'
 import tw from 'twrnc'
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
@@ -6,6 +6,8 @@ import { GOOGLE_MAPS_APIKEY } from '@env';
 import { useDispatch } from 'react-redux';
 import { useNavigation } from '@react-navigation/native';
 import { setDestination } from '../slices/navSlice';
+import NavFavourites from './NavFavourites';
+import { Icon } from 'react-native-elements';
 
 const NavigateCard = () => {
  const dispatch = useDispatch();
@@ -37,6 +39,22 @@ const NavigateCard = () => {
       enablePoweredByContainer={false}
      />
     </View>
+    <NavFavourites />
+   </View>
+   <View style={tw`flex-row bg-white justify-evenly py-2 mt-auto border-t border-gray-200`}>
+
+    <TouchableOpacity
+     onPress={() => navigation.navigate('RideOptionsCard')}
+     style={tw`flex flex-row justify-between bg-black w-24 px-4 py-3 rounded-full`}>
+     <Icon name='car' type='font-awesome' color="white" size={16} />
+     <Text style={tw`text-white text-center`}>Rides</Text>
+    </TouchableOpacity>
+
+    <TouchableOpacity style={tw`flex flex-row justify-between w-24 px-4 py-3 rounded-full`}>
+     <Icon name='fast-food-outline' type='ionicon' color="black" size={16} />
+     <Text style={tw`text-center`}>Eats</Text>
+    </TouchableOpacity>
+
    </View>
   </SafeAreaView>
  )
